5cab0577f7113726f01331da777f51b206264032
Harrison Davidson <125293541+HarrisonDavidson@users.noreply.github.com>
2023-08-30 14:45:47 -0400

    printing movies to console again, but this time I saved my SQL tables

---
+++ SQLQuery1.sql
@@ -0,0 +1,64 @@
+﻿USE [master]
+GO
+
+IF db_id('HomeLibrary') IS NULL
+  CREATE DATABASE [HomeLibrary]
+GO
+
+USE [HomeLibrary]
+GO
+
+DROP TABLE IF EXISTS Movies;
+DROP TABLE IF EXISTS Shows;
+DROP TABLE IF EXISTS Music;
+DROP TABLE IF EXISTS VideoGames;
+DROP TABLE IF EXISTS Books;
+
+CREATE TABLE Movies (
+    [Id]           INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name]         VARCHAR (50)  NOT NULL,
+    [YearReleased] VARCHAR (50) NOT NULL,
+    [Director]     VARCHAR (50) NOT NULL,
+    [Image]        VARCHAR (50)  NULL,
+    [Format]       VARCHAR (50)  NULL,
+    [Notes]        TEXT          NULL,
+);
+
+CREATE TABLE Shows (
+    [Id]           INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name]         VARCHAR (50)  NOT NULL,
+    [YearReleased] VARCHAR (255) NOT NULL,
+    [Image]        VARCHAR (50)  NULL,
+    [Format]       VARCHAR (50)  NULL,
+    [Notes]        TEXT          NULL,
+);
+
+CREATE TABLE Music (
+    [Id]           INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name]         VARCHAR (50)  NOT NULL,
+    [Artist]       VARCHAR (50)  NOT NULL,
+    [YearReleased] VARCHAR (255) NOT NULL,
+    [Image]        VARCHAR (50)  NULL,
+    [Format]       VARCHAR (50)  NULL,
+    [Notes]        TEXT          NULL,
+);
+
+CREATE TABLE VideoGames (
+    [Id]           INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name]         VARCHAR (50) NOT NULL,
+    [Studio]       VARCHAR (50) NOT NULL,
+    [YearReleased] VARCHAR (50) NOT NULL,
+    [Image]        VARCHAR (50)  NULL,
+    [Format]       VARCHAR (50)  NOT NULL,
+    [Notes]        TEXT          NULL,
+);
+
+CREATE TABLE Books (
+    [Id]           INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name]         VARCHAR (50) NOT NULL,
+    [Author]       VARCHAR (50) NOT NULL,
+    [YearReleased] VARCHAR (50) NOT NULL,
+    [Image]        VARCHAR (50)  NULL,
+    [Format]       VARCHAR (50)  NOT NULL,
+    [Notes]        TEXT          NULL,
+);
---
+++ SQLQuery2.sql
@@ -0,0 +1,9 @@
+﻿USE [HomeLibrary]
+GO
+
+set identity_insert [Movies] on
+insert into [Movies] ([Id], [Name], [YearReleased], [Director], [Image], [Format], [Notes])
+values (1, 'Full Metal Jacket', '1987', 'Stanley Kubrick', 'www', 'DVD', 'My 2nd favorite movie'), 
+(2, 'The Lord of the Rings: The Fellowship of the Ring', '2001', 'Peter Jackson', 'www', 'DVD', 'My all time favorite movie'),
+(3, 'Pulp Fiction', '1994', 'Quentin Tarantino', 'www', 'VHS', 'My old favorite movie')
+set identity_insert [Movies] off
